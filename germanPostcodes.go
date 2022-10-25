8978a649d98c1078e454645f3781c8947c36c329
nicobock <nico.bock@graphmasters.net>
2022-10-25 09:03:13 +0200

    init commit

--- germanPostcodes.go
+++ germanPostcodes.go
@@ -21,22 +21,60 @@ type ZipcodeDateset struct {
 	Latitude      string `json:"latitude"`
 	Longitude     string `json:"longitude"`
 }
+type ZipcodeFinder struct {
+	ZipcodeMap map[string][]*ZipcodeDateset
+}
 
-func GetStateForPostCode(postcode string) (state, error) {
+func InitZipcodeFinder() (ZipcodeFinder, error) {
+	this := ZipcodeFinder{}
+	var err error
+	this.ZipcodeMap, err = GetZipCodeMap()
+	if err != nil {
+		return this, errors.Wrap(err, "Failed to loadZipcodeDate")
+	}
+	return this, nil
+}
+
+func GetZipCodeMap() (map[string][]*ZipcodeDateset, error) {
 
 	data, err := os.ReadFile("DE/zipcodes.de.json")
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to Load ZipcodeJSON")
+		return nil, errors.Wrap(err, "Failed to Load ZipcodeJSON")
 	}
 	GermanZipcodes := make([]*ZipcodeDateset, 0)
 	err = json.Unmarshal(data, GermanZipcodes)
 	if err != nil {
-		return "", errors.Wrap(err, "Failed to unmarshall ZipcodeJSON")
+		return nil, errors.Wrap(err, "Failed to unmarshall ZipcodeJSON")
 	}
 
-	GermanZipcodeMap := make(map[string]*ZipcodeDateset)
-	for _, Zipcode := range GermanZipcodes {
-		ger
+	GermanZipcodeMap := make(map[string][]*ZipcodeDateset)
+	for _, ZipcodeData := range GermanZipcodes {
+		if _, contains := GermanZipcodeMap[ZipcodeData.Zipcode]; !contains {
+			GermanZipcodeMap[ZipcodeData.Zipcode] = make([]*ZipcodeDateset, 0)
+		}
+		GermanZipcodeMap[ZipcodeData.Zipcode] = append(GermanZipcodeMap[ZipcodeData.Zipcode], ZipcodeData)
 	}
 
+	return GermanZipcodeMap, nil
+
+}
+
+func GetStateFromZipCode(postcode string) (state, error) {
+	GermanZipcodeMap, err := GetZipCodeMap()
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to load ZicodeData")
+	}
+
+	if ZipcodeData, containts := GermanZipcodeMap[postcode]; containts {
+		return state(ZipcodeData[0].State), nil
+	}
+	return "", errors.New("Zipcode not found")
+}
+
+func (z ZipcodeFinder) GetStateFromZipCode(postcode string) (state, error) {
+
+	if ZipcodeData, containts := z.ZipcodeMap[postcode]; containts {
+		return state(ZipcodeData[0].State), nil
+	}
+	return "", errors.New("Zipcode not found")
 }
